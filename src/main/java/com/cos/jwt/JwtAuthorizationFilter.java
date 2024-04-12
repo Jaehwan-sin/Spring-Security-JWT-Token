ba8fd44fc3757841d589c546f5cb2115ddf992c3
jae hwan shin <84274952+Jaehwan-sin@users.noreply.github.com>
2024-04-12 16:46:39 +0900

    JWT 토큰 로그인 검증 / 서비스

--- src/main/java/com/cos/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/cos/jwt/JwtAuthorizationFilter.java
@@ -2,7 +2,7 @@ package com.cos.jwt;
 
 import com.auth0.jwt.JWT; 
 import com.auth0.jwt.algorithms.Algorithm;
-import com.cos.jwt.config.auth.PrincipalDetails;
+import com.cos.jwt.config.auth.PrincipalDetails; 
 import com.cos.jwt.model.User;
 import com.cos.jwt.repository.UserRepository; 
 import org.springframework.security.authentication.AuthenticationManager;
